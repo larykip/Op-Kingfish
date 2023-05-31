db735eff1c6af897234c8f4725c0a1b2b42fa4d9
larykip <hillarykiprotich35@gmail.com>
2023-05-31 21:34:28 +0300

    added a dummy static page and created the static folder initialized the static folder on the settings.py created a model named Feature that has 2 database columns; name and details made makemigrations and migrate the database using the model authenticated the admin superuser account registered the Feature model on admin.py in order to access it accessed the database on the view, created a database object and passed the datato the static(html) used a for loop to loop through the data in the database and display it

--- kingfish/views.py
+++ kingfish/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    features = Feature.objects.all()
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     words = request.POST['words']
