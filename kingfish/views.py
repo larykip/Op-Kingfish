0b23b0779d69ff6cc35daa30dde29d8790675456
larykip <hillarykiprotich35@gmail.com>
2023-06-01 18:09:32 +0300

    created a login and logout html form created the corresponding views and form for registering new users together with the functionality once new users are registered, they are directed to the login page on the login page, the logged in username is displayed on top left There is a corresponding log out buttons which on clicked redirects to the same homepage and a login button appears

--- kingfish/views.py
+++ kingfish/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Feature
 
@@ -11,3 +13,46 @@ def counter(request):
     words = request.POST['words']
     count = len(words.split())
     return render(request, 'counter.html', {'words': count})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+
+        if password == password1:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists!')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already used!')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password = password)
+                user.save()
+                return redirect('login')
+        else:
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Incorrect credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
